9068e2c894eb6cda586a631bd8ee4e66a9f9280d
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-16 18:32:54 -0400

    created run file and updated return_book method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,13 @@ class User < ActiveRecord::Base
   has_many :books, through: :user_books
 
   def check_out(book, due_date)
-    checkout = UserBook.find_by(due_date: due_date,
-    overdue: false, user_id: self.id, book_id: book.id)
-    if checkout == nil
-      UserBook.create(due_date: due_date, overdue:false, user_id: self.id, book_id: book.id)
-    else
-      checkout.destroy
-      UserBook.create(due_date: due_date, returned: false, overdue:false, user_id: self.id, book_id: book.id)
-    end
+    checkout = UserBook.find_or_create_by(due_date: due_date, returned: false, overdue: false, user_id: self.id, book_id: book.id)
   end
 
-
   def return_book(book)
-    to_return = UserBook.find_by(user_id: self.id, book_id: book.id)
+    to_return = UserBook.find_by(user_id: self.id, book_id: book.id, returned: false)
     to_return.returned = true
-    to_return
+    to_return.save
   end
 
 end
